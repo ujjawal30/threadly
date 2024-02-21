636209771a24d4aa918b4b4022c1b8a239914e3f
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-02-22 02:19:44 +0530

    Styled Right Pane

--- components/sections/RightPane.tsx
+++ components/sections/RightPane.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function RightPane() {
-  return <div>RightPane</div>;
+  return (
+    <section className="custom-scrollbar rightpane">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-1">Right Sidebar</h3>
+      </div>
+    </section>
+  );
 }
 
 export default RightPane;
