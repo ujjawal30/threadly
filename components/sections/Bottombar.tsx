d9cbd72fb9d5761de17be31949856115841f6c3f
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-02-22 02:19:26 +0530

    Designed Bottom nav

--- components/sections/Bottombar.tsx
+++ components/sections/Bottombar.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
 import React from "react";
 
 function Bottombar() {
-  return <div>Bottombar</div>;
+  const pathname = usePathname();
+
+  const isActive = (route: string) =>
+    (pathname.includes(route) && route.length > 1) || pathname === route;
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => (
+          <Link
+            href={link.route}
+            key={link.label}
+            className={`bottombar_link ${
+              isActive(link.route) && "bg-primary-500"
+            }`}
+          >
+            <link.icon className="text-light-1" size={24} />
+            <p className="text-subtle-medium text-light-1 max-sm:hidden">
+              {link.label.split(/\s+/)[0]}
+            </p>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Bottombar;
