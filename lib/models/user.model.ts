5046ff9f60e82575b4e52b8a9a30ff1a47741ee5
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-02-25 03:01:40 +0530

    Created user model

---
+++ lib/models/user.model.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  image: String,
+  bio: String,
+  onboarded: { type: Boolean, default: false },
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  ],
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
