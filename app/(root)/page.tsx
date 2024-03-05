adef4396bc606c8689d10e493e4582790713a66f
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-03-06 00:23:50 +0530

    Fetch all threads action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import { fetchThreads } from "@/lib/actions/thread.action";
+
+export default async function Home() {
+  const threadsData = await fetchThreads();
+
+  console.log("threadsData :>> ", threadsData);
+
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
+      <section className="mt-10 flex flex-col gap-10">
+        <p className="no-result">No threads found</p>
+      </section>
     </div>
   );
 }
--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToMongoDB } from "../mongodb";
@@ -24,7 +25,39 @@ export const createThread = async (data: ThreadData, path: string) => {
     await User.findByIdAndUpdate(author, {
       $push: { threads: newThread._id },
     });
+
+    revalidatePath(path);
   } catch (error: any) {
     throw new Error("Unable to post new thread:", error.message);
+  }
+};
+
+export const fetchThreads = async () => {
+  try {
+    await connectToMongoDB();
+
+    const threads = await Thread.find({
+      parentThread: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(0)
+      .limit(20)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "comments",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentThread image",
+        },
+      });
+
+    const threadsCount = await Thread.countDocuments({
+      parentThread: { $in: [null, undefined] },
+    });
+
+    return { threads, threadsCount };
+  } catch (error: any) {
+    throw new Error("Unable to fetch threads: ", error.message);
   }
 };
