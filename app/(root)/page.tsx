763aae732641b8825fd6a789e3cfbadd09ef839e
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-03-08 02:29:05 +0530

    Creaded thread card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,39 @@
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchThreads } from "@/lib/actions/thread.action";
+import { currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
-  const threadsData = await fetchThreads();
+  const user = await currentUser();
+  const { threads, threadsCount } = await fetchThreads();
 
-  console.log("threadsData :>> ", threadsData);
+  if (!user) return null;
+  console.log("threads :>> ", threads);
+  console.log("threads :>> ", JSON.parse(JSON.stringify(threads)));
 
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
       <section className="mt-10 flex flex-col gap-10">
-        <p className="no-result">No threads found</p>
+        {threads.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          threads?.map((thread) => (
+            <ThreadCard
+              key={thread?._id}
+              id={thread.id}
+              currentUser={user?.id}
+              content={thread?.content}
+              author={{
+                id: thread?.author?.id,
+                name: thread?.author?.name,
+                image: thread?.author?.image,
+              }}
+              createdAt={thread?.createdAt}
+              parentThread={thread?.parentThread}
+              comments={thread?.comments}
+            />
+          ))
+        )}
       </section>
     </div>
   );
---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { ObjectId } from "mongoose";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import {
+  TbHeart,
+  TbMessage,
+  TbMessageCircle,
+  TbSend,
+  TbShare2,
+  TbShare3,
+} from "react-icons/tb";
+
+interface Props {
+  id: string;
+  currentUser: string;
+  content: string;
+  author: {
+    name: string;
+    image: string;
+    id: string;
+  };
+  createdAt: Date;
+  parentThread: string | null;
+  comments: {
+    author: {
+      image: string;
+    };
+  }[];
+}
+
+async function ThreadCard({
+  id,
+  currentUser,
+  content,
+  author,
+  createdAt,
+  parentThread,
+  comments,
+}: Props) {
+  return (
+    <article className="flex flex-col w-full rounded-xl bg-dark-2 p-8">
+      <div className="flex items-start justify-between">
+        <div className="flex w-full flex-1 gap-5">
+          <div className="flex flex-col items-center">
+            <Link href={`/profile/${author.id}`} className="relative h-10 w-10">
+              <Image
+                src={author.image}
+                alt="Profile Photo"
+                fill
+                className="cursor-pointer rounded-full"
+              />
+            </Link>
+
+            <div className="thread-card_bar"></div>
+          </div>
+
+          <div className="flex flex-col w-full">
+            <Link href={`/profile/${author.id}`} className="w-fit">
+              <h4 className="cursor-pointer text-light-1 text-small-semibold">
+                {author.name}
+              </h4>
+            </Link>
+
+            <p className="mt-2 text-light-2 text-small-regular">{content}</p>
+
+            <div className="mt-5 flex flex-col gap-3">
+              <div className="flex gap-3 text-gray-600">
+                <TbHeart
+                  size={20}
+                  className="cursor-pointer hover:text-light-3"
+                />
+                <TbMessageCircle
+                  size={20}
+                  className="cursor-pointer hover:text-light-3"
+                />
+                <TbShare3
+                  size={20}
+                  className="cursor-pointer hover:text-light-3"
+                />
+                <TbSend
+                  size={20}
+                  className="cursor-pointer hover:text-light-3"
+                />
+              </div>
+
+              {comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p>
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default ThreadCard;
