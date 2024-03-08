b48a59f0282e02f90b778c4ac58737985f21699a
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-03-09 02:18:17 +0530

    Designed Threads page and comments functionality

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,63 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import Comment from "@/components/forms/Comment";
+import { fetchThreadById } from "@/lib/actions/thread.action";
+import { fetchUser } from "@/lib/actions/user.action";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+
+interface Props {
+  params: { id: string };
+}
+
+async function Page({ params }: Props) {
+  const user = await currentUser();
+
+  if (!params.id || !user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const thread = await fetchThreadById(params.id);
+
+  return (
+    <section className="relative">
+      <ThreadCard
+        key={thread?._id}
+        id={thread?._id}
+        currentUser={user?.id}
+        content={thread?.content}
+        author={thread?.author}
+        createdAt={thread?.createdAt}
+        parentThread={thread?.parentThread}
+        comments={thread?.comments}
+      />
+
+      <div className="mt-7">
+        <Comment
+          threadId={thread?._id}
+          userId={JSON.parse(JSON.stringify(userInfo?._id))}
+          userImg={userInfo?.image}
+        />
+      </div>
+
+      <div className="flex flex-col gap-4 mt-10">
+        {thread.comments.map((comment: any) => (
+          <ThreadCard
+            key={comment?._id}
+            id={comment?._id}
+            currentUser={user?.id}
+            content={comment?.content}
+            author={comment?.author}
+            createdAt={comment?.createdAt}
+            parentThread={comment?.parentThread}
+            comments={comment?.comments}
+            isComment={true}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Page;
---
+++ components/forms/Comment.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { CommentValidation } from "@/lib/validations/thread";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import Image from "next/image";
+import { postComment } from "@/lib/actions/thread.action";
+
+interface Props {
+  threadId: string;
+  userId: string;
+  userImg: string;
+}
+
+function Comment({ threadId, userId, userImg }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+
+  const handleSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    console.log("Thread :>> ", values);
+
+    await postComment(threadId, values.thread, userId, pathname);
+
+    form.reset();
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="comment-form">
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-2 w-full">
+              <FormLabel>
+                <Image
+                  src={userImg}
+                  alt="Profile Photo"
+                  height={48}
+                  width={48}
+                  className="rounded-full object-cover"
+                />
+              </FormLabel>
+              <FormControl className="border-none bg-transparent">
+                <Input
+                  type="text"
+                  placeholder="Comment..."
+                  className="no-focus text-light-1 outline-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="comment-form_btn">
+          Reply
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export default Comment;
