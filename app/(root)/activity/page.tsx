5adb3b86ab55051eb597c6e5a1b785ef0bea869a
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-03-13 02:33:09 +0530

    config changes

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -40,7 +40,7 @@ async function Page() {
             </Link>
           ))
         ) : (
-          <p>No activity yet</p>
+          <p className="!text-base-regular text-light-3">No activity yet</p>
         )}
       </section>
     </section>
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "icon-library.com",
       },
     ],
   },
